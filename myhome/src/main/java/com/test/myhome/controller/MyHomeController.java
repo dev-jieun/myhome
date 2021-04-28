9131feaa8ed90dcfc56f2109ceec12671c1ead07
Jenny <Jenny@172.30.17.188>
2021-04-28 19:09:11 +0900

    setting layout

--- myhome/src/main/java/com/test/myhome/controller/MyHomeController.java
+++ myhome/src/main/java/com/test/myhome/controller/MyHomeController.java
@@ -1,4 +1,4 @@
-package com.test.myhone.controller;
+package com.test.myhome.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
