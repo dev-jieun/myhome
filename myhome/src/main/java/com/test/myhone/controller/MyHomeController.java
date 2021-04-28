d72b2d669b6763c5ccf607f663dd7b2238a56543
Jenny <Jenny@172.30.17.188>
2021-04-28 17:08:22 +0900

    commit

---
+++ myhome/src/main/java/com/test/myhone/controller/MyHomeController.java
@@ -0,0 +1,13 @@
+package com.test.myhone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyHomeController {
+
+	@RequestMapping
+	public String index() {
+		return "index";
+	}
+}
